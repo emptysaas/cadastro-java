f5e12a2caf7437fe4ff1e1935451f13c9265151a
emptysaas <luiz.gus.ms@gmail.com>
2025-04-30 01:46:23 -0300

    chore: Implementa a versao DTO e Mapper manualmente

--- CadastroDeNinja/src/main/java/com/java10x/CadastroDeNinja/Ninjas/NinjaDTO.java
+++ CadastroDeNinja/src/main/java/com/java10x/CadastroDeNinja/Ninjas/NinjaDTO.java
@@ -1,4 +1,23 @@
 package com.java10x.CadastroDeNinja.Ninjas;
 
+import com.java10x.CadastroDeNinja.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaDTO {
+
+    private Long identificador;
+    private String name;
+    private String username;
+    private String mail;
+    private String sex;
+    private int age;
+    private String rank;
+    private MissoesModel missoes;
+
 }
--- CadastroDeNinja/src/main/java/com/java10x/CadastroDeNinja/Ninjas/NinjaMapper.java
+++ CadastroDeNinja/src/main/java/com/java10x/CadastroDeNinja/Ninjas/NinjaMapper.java
@@ -1,4 +1,33 @@
 package com.java10x.CadastroDeNinja.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO) {
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setIdentificador(ninjaDTO.getIdentificador());
+        ninjaModel.setAge(ninjaDTO.getAge());
+        ninjaModel.setMail(ninjaDTO.getMail());
+        ninjaModel.setName(ninjaDTO.getName());
+        ninjaModel.setUsername(ninjaDTO.getUsername());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setSex(ninjaDTO.getSex());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel) {
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setIdentificador(ninjaModel.getIdentificador());
+        ninjaDTO.setAge(ninjaModel.getAge());
+        ninjaDTO.setMail(ninjaModel.getName());
+        ninjaDTO.setName(ninjaModel.getName());
+        ninjaDTO.setUsername(ninjaModel.getUsername());
+        ninjaDTO.setRank(ninjaModel.getRank());
+        ninjaDTO.setSex(ninjaModel.getSex());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
+        return ninjaDTO;
+    }
 }
